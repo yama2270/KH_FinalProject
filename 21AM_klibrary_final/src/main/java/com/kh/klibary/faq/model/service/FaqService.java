04843222bc0bd2fd5f12f13f6302e96f392deb1b
hjlukeskywalker <iamsokawai@gmail.com>
2021-07-14 10:43:37 +0900

    #hj faq코딩

---
+++ 21AM_klibrary_final/src/main/java/com/kh/klibary/faq/model/service/FaqService.java
@@ -0,0 +1,9 @@
+package com.kh.klibary.faq.model.service;
+
+import java.util.List;
+
+import com.kh.klibary.faq.model.vo.Faq;
+
+public interface FaqService {
+	List<Faq> selectFaqList();
+}
---
+++ 21AM_klibrary_final/src/main/java/com/kh/klibary/faq/model/vo/Faq.java
@@ -0,0 +1,11 @@
+package com.kh.klibary.faq.model.vo;
+
+import lombok.Data;
+
+@Data
+public class Faq {
+	private int faq_No;
+	private String faq_Category;
+	private String faq_Content;
+	private String faq_Answer;
+}
