dee03bd994693489a407fe51046ecec6c95896be
승혁 <agkawg@gmail.com>
2021-07-14 14:26:46 +0900

    #승혁 admin jsp파일 경로수정

--- 21AM_klibrary_final/src/main/java/com/kh/klibrary/admin/book/model/controller/BookController.java
+++ 21AM_klibrary_final/src/main/java/com/kh/klibrary/admin/book/model/controller/BookController.java
@@ -1,8 +1,13 @@
 package com.kh.klibrary.admin.book.model.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class BookController {
 
+	@RequestMapping("/admin/book/registerBook.do")
+	public String registerBook() {
+		return "admin/book/registerBook";
+	}
 	
-	 
 }
  
