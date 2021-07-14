432bdf74a21cf58c88d391c167045242bf65b5bb
hjlukeskywalker <iamsokawai@gmail.com>
2021-07-15 01:38:19 +0900

    #호준 qna jsp와 controller주소매핑 및 맵퍼파일추가

---
+++ 21AM_klibrary_final/src/main/java/com/kh/klibrary/qna/modelvo/Qna.java
@@ -0,0 +1,16 @@
+package com.kh.klibrary.qna.modelvo;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data//  롬복
+public class Qna {
+	private int qna_No;
+	private String user_Id;
+	private String qna_Title;
+	private String qna_Img;
+	private Date qna_Date;
+	private int qna_Count;
+	private String qna_State;
+}
