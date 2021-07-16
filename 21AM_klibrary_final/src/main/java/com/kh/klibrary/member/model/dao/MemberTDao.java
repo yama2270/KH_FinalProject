c6aca64dd350b7a3300a17db909e124e8bcd08a1
chan-gyu <ecs1205@naver.com>
2021-07-16 19:39:11 +0900

    메뉴 수정 및 커밋

--- 21AM_klibrary_final/src/main/java/com/kh/klibrary/member/model/dao/MemberTDao.java
+++ 21AM_klibrary_final/src/main/java/com/kh/klibrary/member/model/dao/MemberTDao.java
@@ -1,11 +1,15 @@
 package com.kh.klibrary.member.model.dao;
 
+import java.util.List;
 import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 
+import com.kh.klibrary.member.model.vo.LendingHistory;
 import com.kh.klibrary.member.model.vo.MemberT;
 
 public interface MemberTDao {
 	MemberT selectMember(SqlSessionTemplate session, Map m1);
+	int updateMember(SqlSessionTemplate session, MemberT m);
+	List<LendingHistory> selectLHList(SqlSessionTemplate session, String userId);
 }
