3f84d845261de47a475526b1d51a5b7aaabf4253
chan-gyu <ecs1205@naver.com>
2021-07-22 11:06:21 +0900

    수정

--- 21AM_klibrary_final/java/com/kh/klibrary/faq/controller/FaqController.java
+++ 21AM_klibrary_final/java/com/kh/klibrary/faq/controller/FaqController.java
@@ -1,5 +1,6 @@
 package com.kh.klibrary.faq.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
