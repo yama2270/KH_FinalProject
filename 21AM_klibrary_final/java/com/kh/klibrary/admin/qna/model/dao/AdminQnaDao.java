4d26a0ddfa6a6b46c1b555102ff3bda2ee12ea8d
승혁 <agkawg@gmail.com>
2021-07-28 14:39:31 +0900

    #승혁 관리자문의사항 게시글리스트,상세보기,조회수증가,선택삭제,검색 구현

--- 21AM_klibrary_final/java/com/kh/klibrary/admin/qna/model/dao/AdminQnaDao.java
+++ 21AM_klibrary_final/java/com/kh/klibrary/admin/qna/model/dao/AdminQnaDao.java
@@ -9,7 +9,7 @@ import com.kh.klibrary.qna.model.vo.Qna;
 
 public interface AdminQnaDao {
 	List<Qna> selectQnaList(SqlSessionTemplate session, int cPage, int numPerpage);
-	int selectQnaCount(SqlSessionTemplate session) ;
+	int selectQnaCount(SqlSessionTemplate session);
 	Qna selectQnaView(SqlSessionTemplate session, int qnaNo);
 	int updateQnaCount(SqlSessionTemplate session, int qnaNo);
 	int deleteQna(SqlSessionTemplate session, String qnaNo);
